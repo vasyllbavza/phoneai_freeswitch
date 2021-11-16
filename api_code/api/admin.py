4b3caf72056e77459d5d47a89bb2ffd37a94544d
redtomato0129 <redtomato0129@gmail.com>
2021-11-16 19:31:14 +0600

    model added

--- api_code/api/admin.py
+++ api_code/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.models import (
+    CallLog,
+    CallKey,
+)
+
+class CallLogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'number', 'uuid', 'status', 'created_at', 'updated_at')
+
+admin.site.register(CallLog, CallLogAdmin)
+
+class CallKeyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'call', 'parent', 'keys', 'level', 'processed', 'created_at', 'updated_at')
+
+admin.site.register(CallKey, CallKeyAdmin)
