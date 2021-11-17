1094eaa8bb9ce924437daf27911e7d07f06dc9f6
redtomato0129 <redtomato0129@gmail.com>
2021-11-17 16:57:12 +0600

    hanup button

--- api_code/api/admin.py
+++ api_code/api/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.conf.urls import url
+from django.utils.html import format_html
+from django.urls import reverse
+from django.shortcuts import get_object_or_404,redirect
 
 # Register your models here.
 from api.models import (
@@ -8,18 +12,47 @@ from api.models import (
 from api.views import freeswitch_execute
 
 class CallLogAdmin(admin.ModelAdmin):
-    list_display = ('id', 'number', 'uuid', 'status', 'created_at', 'updated_at')
+    list_display = ('id', 'number', 'uuid', 'status', 'created_at', 'updated_at','hangup_link')
 
     actions = ['hangup_call']
 
     @admin.action(description='Hangup Call')
     def hangup_call(self, request, queryset):
-        # queryset.update(status='p')
         for qs in queryset:
             cmd = 'bgapi'
             args = f"uuid_kill {qs.uuid}"
             result = freeswitch_execute(cmd,args)
             print(result)
+
+    def hangup_link(self, obj):
+        return format_html(
+            '<a class="button" href="{}">Hangup</a>&nbsp;',
+            reverse('admin:hangup-call', args=[obj.pk]),
+        )
+
+    hangup_link.short_description = 'Hangup Call'
+    hangup_link.allow_tags = True
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            url(
+                r'^hangup_call_action/(?P<pk>\d+)/$',
+                self.admin_site.admin_view(self.hangup_view),
+                name='hangup-call',
+            ),
+        ]
+        return custom_urls + urls
+
+    # @permission_required('CallLog.can_change')
+    def hangup_view(self, request, pk):
+        obj = get_object_or_404(CallLog, pk=pk)
+        cmd = 'bgapi'
+        args = f"uuid_kill {obj.uuid}"
+        result = freeswitch_execute(cmd,args)
+        print(result)
+        print(args)
+        return redirect('/api/admin/api/calllog/')
 
 admin.site.register(CallLog, CallLogAdmin)
 
