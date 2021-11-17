3568bd5d823bcffb5db37d882f27861b43b95cc1
redtomato0129 <redtomato0129@gmail.com>
2021-11-17 16:19:27 +0600

    hangup action button

--- api_code/api/admin.py
+++ api_code/api/admin.py
@@ -5,9 +5,21 @@ from api.models import (
     CallLog,
     CallKey,
 )
+from api.views import freeswitch_execute
 
 class CallLogAdmin(admin.ModelAdmin):
     list_display = ('id', 'number', 'uuid', 'status', 'created_at', 'updated_at')
+
+    actions = ['hangup_call']
+
+    @admin.action(description='Hangup Call')
+    def hangup_call(self, request, queryset):
+        # queryset.update(status='p')
+        for qs in queryset:
+            cmd = 'bgapi'
+            args = f"uuid_kill {qs.uuid}"
+            result = freeswitch_execute(cmd,args)
+            print(result)
 
 admin.site.register(CallLog, CallLogAdmin)
 
