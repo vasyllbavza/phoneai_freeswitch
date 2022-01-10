0d4846320118e5aa458f0264c26076be5dab74db
redtomato0129 <redtomato0129@gmail.com>
2022-01-10 17:47:48 +0600

    update dialstring

--- api_code/api/views.py
+++ api_code/api/views.py
@@ -104,8 +104,9 @@ class MakeCallView(APIView):
                 call.save()
                 call_id = call.id
 
+        call_menu_id = 0
         cmd = 'bgapi'
-        phonenumber_info = "is_new_call=%s,phoneai_number_id=%s,phoneai_call_id=%s,call_menu_id=0" % (str(is_new_call),str(number.id),str(call_id))
+        phonenumber_info = "is_new_call=%s,phoneai_number_id=%s,phoneai_call_id=%s,call_menu_id=%s" % (str(is_new_call),str(number.id),str(call_id), str(call_menu_id))
         callParams = "{%s,ignore_early_media=true,origination_caller_id_name=phoneAI,origination_caller_id_number=%s,origination_uuid=%s}" % (phonenumber_info,caller_id,call_uuid)
         args = "originate %ssofia/gateway/58e29eb4-bc1e-4c3d-bf30-25ff961b1b99/69485048*%s &lua(phoneai.lua)" %(callParams,dial_number)
         print( "%s %s" %(cmd,args) )
