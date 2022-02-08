b7c33f855864844de923ff4c84ed05d87eb7469c
redtomato0129 <redtomato0129@gmail.com>
2022-02-08 11:09:23 +0600

    update api

--- api_code/api/utils.py
+++ api_code/api/utils.py
@@ -3,6 +3,10 @@ class TreeNode(dict):
         super().__init__()
         self.__dict__ = self
         self.id = id
+        self.info = {
+            "parent_text": parent_text,
+            "keys_to_reach": keys_to_reach
+        }
         self.parent_text = parent_text
         self.keys_to_reach = keys_to_reach
         self.key = key
