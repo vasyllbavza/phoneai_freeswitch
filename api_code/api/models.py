ca37d1be1ed4677c1f91236adab6b0c0d455b351
redtomato0129 <redtomato0129@gmail.com>
2022-02-11 16:02:11 +0600

    api update

--- api_code/api/models.py
+++ api_code/api/models.py
@@ -135,7 +135,7 @@ class AgentCallLog(models.Model):
         """audio player tag for admin"""
         if self.audio_file:
             file_url = settings.MEDIA_URL + str(self.audio_file)
-            player_string = '<a href="%s" controls>Recording</a>' % (file_url)
+            player_string = '<a href="%s">Recording</a>' % (file_url)
             return player_string
 
     audio_file_player.allow_tags = True
