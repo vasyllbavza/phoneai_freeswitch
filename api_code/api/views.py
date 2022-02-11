dfd9781fe7cae727900f3a889e7e64e57b01ce43
redtomato0129 <redtomato0129@gmail.com>
2022-02-11 13:10:12 +0600

    api update

--- api_code/api/views.py
+++ api_code/api/views.py
@@ -315,11 +315,11 @@ class MakeCallSubMenuView(APIView):
             content['fs_output'] = ""
             return Response(content)
 
-        if menu.completed == True:
-            content['status'] = "fail"
-            content['message'] = "menu is completed already."
-            content['fs_output'] = ""
-            return Response(content)
+        # if menu.completed == True:
+        #     content['status'] = "fail"
+        #     content['message'] = "menu is completed already."
+        #     content['fs_output'] = ""
+        #     return Response(content)
 
         caller_id = '14582037530'
         number = menu.call.number
@@ -337,6 +337,10 @@ class MakeCallSubMenuView(APIView):
         callParams = "{%s,ignore_early_media=true,origination_caller_id_name=phoneAI,origination_caller_id_number=%s,origination_uuid=%s}" % (phonenumber_info,caller_id,call_uuid)
         args = "originate %ssofia/gateway/58e29eb4-bc1e-4c3d-bf30-25ff961b1b99/69485048*%s &lua(phoneai_go.lua)" %(callParams,dial_number)
         print( "%s %s" %(cmd,args) )
+        content["command"] = cmd
+        content["args"] = args
+        return Response(content)
+
         result = freeswitch_execute(cmd,args)
         if result['status'] < 1:
             content['status'] = "fail"
